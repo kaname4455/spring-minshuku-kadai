c0e8262f2e047d0d14384c20763391bdb44efe5d
kaname4455 <kanamebaseball2721@gmail.com>
2025-06-06 20:46:43 +0900

    Delete resources directory

--- resources/static/js/stripe.js
+++
@@ -1,8 +0,0 @@
-const stripe = Stripe('pk_test_51RWYWAQbHWgfbt7kQ9IyyehcHsZVTYxfN7WSUhZSpYuhQxGXpCJJTTccgOf4YzW236HmDBGiZVA9Ry2wYgx8ECIQ00IRmLl1pf');
-const paymentButton = document.querySelector('#paymentButton');
-
-paymentButton.addEventListener('click', () => {
- stripe.redirectToCheckout({
-   sessionId: sessionId
- })
-});
